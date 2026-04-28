2b61d084f21c25e05a80e07d2857f4ddb9095801
sonu kumar <skrishnu89@gmail.com>
2026-04-28 20:50:37 +0530

    api_key

--- project.py
+++ project.py
@@ -1,7 +1,13 @@
 from openai import OpenAI
 import streamlit as st
 
-client=OpenAI()
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=st.secrets["OPENROUTER_API_KEY"],
+    base_url="https://openrouter.ai/api/v1"
+)
 st.set_page_config(page_title="Ai Blog", page_icon="🤷‍♂️")
 
 st.title("AI Blog Generator")
@@ -10,10 +16,10 @@ user_input=st.text_input("enter your topic!")
 word=st.slider("word Count",100,500,150)
 style = st.selectbox("Blog Style", ["General", "Technical", "SEO", "Creative"])
 
-system_prompt=f"You are a professional blog writer. write a blog  on {user_input} of at least {word} words with a proper introduction, and have a {style} side blog reference, and conclusion. Keep the language simple and engaging."
 
 
-def blog(user_input):
+def blog(user_input,word,style):
+    system_prompt=f"You are a professional blog writer. write a blog  on {user_input} of at least {word} words with a proper introduction, and have a {style} side blog reference, and conclusion. Keep the language simple and engaging."
     message=[
         {
             "role":"system",
@@ -24,7 +30,7 @@ def blog(user_input):
         }
     ]
     response=client.chat.completions.create(
-        model="",
+        model="nvidia/nemotron-3-nano-30b-a3b:free",
         messages=message
     )
     return response.choices[0].message.content
@@ -35,7 +41,7 @@ output=""
 if st.button("Generate Blog"):
     if user_input:
       with st.spinner("Generating blog..."):
-        result = blog(user_input)
+        result = blog(user_input,word,style)
         st.success("Blog generated successfully!")
         output=result
         st.write(result)
@@ -45,4 +51,4 @@ if st.button("Generate Blog"):
 if st.button("Reset"):
     st.write("")
 
-st.download_button("download_blog",output,file_name="blog.text")
+st.download_button("download_blog",output,file_name="blog.txt")
